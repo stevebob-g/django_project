3ebd11d51405b4ea4bb73797eda663c7cc19dc5d
steve_bob <stevebomda@gmail.com>
2026-02-14 11:23:27 +0100

    Clean repo and apply .gitignore

--- monprojet/settings.py
+++ monprojet/settings.py
@@ -72,13 +72,19 @@ WSGI_APPLICATION = 'monprojet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+from decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Password validation
